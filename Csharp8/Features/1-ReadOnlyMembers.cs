bfd8ab0c944180ae4853a5405e8c2746d6073673
Mostafa Kazemi <m.kazemi.512@gmail.com>
2021-08-05 04:10:41 +0430

    ReadOnly Members tested Default Interdace Tested

---
+++ Csharp8/Features/1-ReadOnlyMembers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp8.Features
+{
+    public struct Rectangle
+    {
+        public double Width { get; set; }
+
+        private double _height;
+
+        public double Height
+        {
+            readonly get { return _height; }
+            set { _height = value; }
+        }
+
+        public readonly double Area()
+        {
+            return Width * Height;
+        }
+    }
+}
---
+++ Csharp8/Features/2-DefaultInterfaceMethod.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace CSharp8.Features
+{
+    public interface IShoppingCart
+    {
+        public static void SetDefaultName(string name)
+        {
+            defaultName = name;
+        }
+
+        private static string defaultName = "default";
+
+
+        void CalculateTotalPrice();
+
+        void CalculateTotalPriceWithTax()
+        {
+            Console.WriteLine($"this is {defaultName} calculated total price with tax");
+        }
+    }
+
+    public class ShoppingCart : IShoppingCart
+    {
+        public void CalculateTotalPrice()
+        {
+            Console.WriteLine("this is default calculated total price");
+        }
+    }
+
+    public class BetterShoppingCart : IShoppingCart
+    {
+        public BetterShoppingCart()
+        {
+            IShoppingCart.SetDefaultName("better shopping cart name");
+        }
+
+
+        public void CalculateTotalPrice()
+        {
+            Console.WriteLine("this is calculated total price");
+        }
+
+        /*public void CalculateTotalPriceWithTax()
+        {
+            Console.WriteLine("this is default calculated total price with tax, override");
+        }*/
+    }
+}
