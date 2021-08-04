24be668e11f696e507f7e92bb3ef0b2b50893a8d
Mostafa Kazemi <m.kazemi.512@gmail.com>
2021-08-05 04:08:19 +0430

    Add project files.

---
+++ Csharp8/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Csharp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
