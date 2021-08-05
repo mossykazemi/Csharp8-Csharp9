07a2c0fc02ba04a4dc615006673f52d59c11d9a8
Mostafa Kazemi <m.kazemi.512@gmail.com>
2021-08-06 04:22:30 +0430

    Create 7-StaticLocalFunction.cs
    Static Local Functions Feature

---
+++ Csharp8/Features/7-StaticLocalFunction.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp8.Features
+{
+    public class Calculator
+    {
+        public int Calculate()
+        {
+            int number_1 = 10;
+            int number_2 = 20;
+            int myValue = 50;
+
+            int result = Sum();
+
+            int Sum() => number_1 + number_2 + myValue;
+
+            return result;
+        }
+
+        public int StaticCalculate()
+        {
+            int number_1 = 30;
+            int number_2 = 50;
+
+            int result = Sum(number_1, number_2);
+
+            static int Sum(int first, int second) => first + second;
+
+            return result;
+        }
+
+    }
+}
