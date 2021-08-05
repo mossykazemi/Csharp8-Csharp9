aab0bd4ef62fcac80d2b2223aca72a74363042e6
Mostafa Kazemi <m.kazemi.512@gmail.com>
2021-08-05 06:17:34 +0430

    Create 05-IndicesAndRanges.cs
    Used Indices And Ranges

---
+++ Csharp8/Features/05-IndicesAndRanges.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+
+namespace CSharp8.Features
+{
+    public static class IndicesAndRanges
+    {
+        public static void Demo()
+        {
+            // myCourses[0] = "ASP.NET Core"
+            // myCourses[4] = "Machine Learning"
+            // var myCourses = new string[] { "ASP.NET Core", "Angular", "Ionic", "Blazor", "Machine learning" };
+            // Console.WriteLine(myCourses[myCourses.Length - 1]);
+            // Console.WriteLine(myCourses[^1]);
+            // Console.WriteLine(myCourses[^2]);
+            var numbers = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            /*foreach (var number in numbers[..^1])
+            {
+                Console.WriteLine(number);
+            }*/
+
+            foreach (var number in numbers[2..])
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
