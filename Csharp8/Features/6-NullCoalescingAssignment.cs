370606680becc233d48968f2b92ed4a844cc34b0
Mostafa Kazemi <m.kazemi.512@gmail.com>
2021-08-05 06:26:55 +0430

    Create 6-NullCoalescingAssignment.cs
    Used Null Coalescing Assignment

---
+++ Csharp8/Features/6-NullCoalescingAssignment.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp8.Features
+{
+    public static class NullCoalescingAssignment
+    {
+        public static void Demo()
+        {
+            var filter = new FilterCoursesViewModel();
+            /*if(filter.Courses == null)
+            {
+                filter.Courses = new List<string>();
+            }*/
+
+            filter.Courses ??= new List<string>();
+
+            filter.Courses.Add("asp.net core");
+            foreach (var course in filter.Courses)
+            {
+                Console.WriteLine(course);
+            }
+        }
+    }
+
+    public class FilterCoursesViewModel
+    {
+        public List<string> Courses { get; set; }
+
+        /*public FilterCoursesViewModel()
+        {
+            this.Courses = new List<string> { "blazor" };
+        }*/
+    }
+}
