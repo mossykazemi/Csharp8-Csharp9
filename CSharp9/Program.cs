210a2419d6ea860167d7cdea80ffade908d719af
Mostafa Kazemi <m.kazemi.512@gmail.com>
2021-08-06 05:50:43 +0430

    Used C# 9 pattern matching improvements

--- CSharp9/Program.cs
+++ CSharp9/Program.cs
@@ -1,1 +1,21 @@
-﻿System.Console.WriteLine("Hello World!"); // Top-Level Statement
+﻿//System.Console.WriteLine("Hello World!"); // Top-Level Statement
+
+namespace CSharp9
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args is not null)
+            {
+
+            }
+            char myCharacter = 'm';
+        }
+
+        public static bool IsLetter(this char c)
+        {
+            return c is >= 'a' and <= 'z' or >= 'A' and <= 'Z';
+        }
+    }
+}
