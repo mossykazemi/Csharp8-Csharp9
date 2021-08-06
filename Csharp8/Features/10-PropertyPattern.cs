b11a60e8b18169a015a72744d4ed3784cb0ffb97
Mostafa Kazemi <m.kazemi.512@gmail.com>
2021-08-06 04:49:25 +0430

    Property Pattern Tested

---
+++ Csharp8/Features/10-PropertyPattern.cs
@@ -0,0 +1,20 @@
+﻿namespace CSharp8.Features
+{
+    public static class PropertyPattern
+    {
+        public static decimal GetTotalPrice(Phone phone) =>
+            phone switch
+            {
+                { Color: "Red" } => phone.Price * 1.05M,
+                { Color: "Green" } => phone.Price * 1.08M,
+                { Color: "Black" } => phone.Price * 1.03M,
+                _ => phone.Price
+            };
+    }
+
+    public class Phone
+    {
+        public string Color { get; set; }
+        public int Price { get; set; }
+    }
+}
--- Csharp8/Program.cs
+++ Csharp8/Program.cs
@@ -7,12 +7,20 @@ namespace Csharp8
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
 
 
-            var res = TuplePattern.RockPaperScissors("rock", "paper");
-            Console.WriteLine(res);
+            //var res = TuplePattern.RockPaperScissors("rock", "paper");
+            //Console.WriteLine(res);
 
+            Phone myPhone = new Phone
+            {
+                Color = "Pink",
+                Price = 10000000
+            };
+
+            var res = PropertyPattern.GetTotalPrice(myPhone);
+            Console.WriteLine(res.ToString("#,0"));
 
             Console.ReadKey();
         }
