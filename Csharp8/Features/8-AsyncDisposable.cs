09a6ae645300e53b59b6ff5badd54f07fd1db7db
Mostafa Kazemi <m.kazemi.512@gmail.com>
2021-08-06 04:32:56 +0430

    Async Disposable Feature Practise

---
+++ Csharp8/Features/8-AsyncDisposable.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CSharp8.Features
+{
+    public class MyProjectContext : DbContext
+    {
+
+    }
+
+
+    public interface IUserService : IAsyncDisposable
+    {
+
+    }
+
+
+    public class UserService : IUserService
+    {
+        private readonly MyProjectContext _context;
+
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            await _context.DisposeAsync();
+        }
+    }
+}
